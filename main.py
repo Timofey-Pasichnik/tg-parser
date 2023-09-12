88ba6bacef6515e6f9431f901fee601b90d71a8d
Timofey Pasichnik <timmofonnius@gmail.com>
2023-09-12 21:33:39 +0300

    TGP-2 refactoring

--- main.py
+++ main.py
@@ -4,14 +4,8 @@ from telethon import TelegramClient
 from natsort import natsorted
 from asyncio import run
 from datetime import datetime, timedelta
+from config import *
 
-api_id = 'someid'
-api_hash = 'somehash'
-chat = 'somechat'
-phone = 'somephone'
-name = 'somename'
-
-DEBUG_MODE = True
 
 list_a = []
 list_tf = []
@@ -19,16 +13,18 @@ list_t = []
 list_f = []
 
 meta_list = [list_a, list_tf, list_t, list_f]
+resulting_list = []
+result = []
 
-#required_post_date = input('Введите дату в формате ДД.ММ.ГГГГ: ')
-required_post_date = '10.09.2023'
 given_date_gmt = datetime.strptime(required_post_date, "%d.%m.%Y")
 given_date_utc = given_date_gmt - timedelta(hours=3)
 next_date_gmt = given_date_gmt + timedelta(days=1)
 next_date_utc = given_date_utc + timedelta(days=1)
 
+
 def list_fill(list_name, text):
     list_name.append(text.replace('\n', ' ').replace('\r', ''))
+
 
 def sort_list(list_name):
     sorted_list = list(set(list_name))
@@ -36,19 +32,43 @@ def sort_list(list_name):
     return sorted_list
 
 
-async def messages_func(name, api_id, api_hash):
+def find_post(client, offset_date, date_condition):
+    for message in client.iter_messages(source_chat, reverse=True, offset_date=offset_date):
+        if isinstance(message,
+                      telethon.tl.patched.Message) and search_string_1 in message.message and not search_string_2 in message.message and date_condition.date() == (
+                message.date + timedelta(hours=3)).date():
+            found_post_id = message.id
+            found_post_time = message.date
+            next_post_time_default = found_post_time + timedelta(days=2)
+            break
+    return found_post_id, found_post_time, next_post_time_default
+
+
+def form_list(given_list, formatted_cpt, text, resulting_list):
+    list_sorted = sort_list(given_list)
+    if len(list_sorted) > 0:
+        resulting_list.append(f"==> {formatted_cpt} {text} ({len(list_sorted)}):")
+        for item_a in list_sorted:
+            multiline_str_a = "\n".join(["{1}".format(i + 1, person_a) for i, person_a in enumerate(list_sorted)])
+        resulting_list.append(multiline_str_a)
+    return resulting_list
+
+
+async def send_comments(name, api_id, api_hash, res):
     async with TelegramClient(name, api_id, api_hash) as client:
-        async for message in client.iter_messages(chat, reverse=True, offset_date=given_date_utc):
-            if isinstance(message, telethon.tl.patched.Message) and 'search_string_1' in message.message and not 'search_string_2' in message.message and given_date_gmt.date() == (message.date + timedelta(hours=3)).date():
-                current_post_id = message.id
-                current_post_time = message.date
-                next_post_time = current_post_time + timedelta(days=2)
-                break
-        async for message in client.iter_messages(chat, reverse=True, offset_date=given_date_utc):
-            if isinstance(message, telethon.tl.patched.Message) and 'search_string_1' in message.message and not 'search_string_2' in message.message and next_date_gmt.date() == (message.date + timedelta(hours=3)).date():
-                next_post_time = message.date
-                break
-        async for message in client.iter_messages(chat, reply_to=int(current_post_id)):
+        for x in range(0, len(res)):
+            if DEBUG_MODE:
+                print(res[x])
+            else:
+                await client.send_message(entity=target_chat, message=res[x])
+
+
+def messages_func(name, api_id, api_hash):
+    with TelegramClient(name, api_id, api_hash) as client:
+        current_post_id, current_post_time, next_post_time = find_post(client, given_date_utc, given_date_gmt)
+        formatted_cpt = (current_post_time + timedelta(hours=3)).replace(tzinfo=None)
+        next_post_time = find_post(client, given_date_utc, next_date_gmt)[1]
+        for message in client.iter_messages(source_chat, reply_to=int(current_post_id)):
             if len(message.message) > 0:
                 if re.search(r'\d', message.message):
                     if message.date < next_post_time:
@@ -60,52 +80,13 @@ async def messages_func(name, api_id, api_hash):
                         list_fill(list_f, message.message)
                     else:
                         list_fill(list_tf, message.message)
-        list_a_sorted = sort_list(list_a)
-        list_tf_sorted = sort_list(list_tf)
-        list_t_sorted = sort_list(list_t)
-        list_f_sorted = sort_list(list_f)
-        if len(list_a_sorted) > 0:
-            if DEBUG_MODE:
-                print(f"==> Отчёты за {(current_post_time + timedelta(hours=3)).replace(tzinfo=None)} от участников ({len(list_a_sorted)}):")
-            else:
-                await client.send_message(entity='target_channel', message=f" ==> Отчёты за {(current_post_time + timedelta(hours=3)).replace(tzinfo=None)} от участников ({len(list_a_sorted)}):")
-            for item_a in list_a_sorted:
-                multiline_str_a = "\n".join(["{1}".format(i + 1, person_a) for i, person_a in enumerate(list_a_sorted)])
-            if DEBUG_MODE:
-                print(multiline_str_a)
-            else:
-                await client.send_message(entity='target_channel', message=multiline_str_a)
-        if len(list_tf_sorted) > 0:
-            if DEBUG_MODE:
-                print(f"==> Отчёты, не прошедшие и по формату и по дате ({len(list_tf_sorted)}):")
-            else:
-                await client.send_message(entity='target_channel', message=f" ==> Отчёты, не прошедшие и по формату и по дате ({len(list_tf_sorted)}):")
-            for item_tf in list_tf_sorted:
-                multiline_str_tf = "\n".join(["{1}".format(i + 1, person_tf) for i, person_tf in enumerate(list_tf_sorted)])
-            if DEBUG_MODE:
-                print(multiline_str_tf)
-            else:
-                await client.send_message(entity='target_channel', message=multiline_str_tf)
-        if len(list_t_sorted) > 0:
-            if DEBUG_MODE:
-                print(f"==> Отчёты, не прошедшие по дате ({len(list_t_sorted)}):")
-            else:
-                await client.send_message(entity='target_channel', message=f" ==> Отчёты, не прошедшие по дате ({len(list_t_sorted)}):")
-            for item_t in list_t_sorted:
-                multiline_str_t = "\n".join(["{1}".format(i + 1, person_t) for i, person_t in enumerate(list_t_sorted)])
-            if DEBUG_MODE:
-                print(multiline_str_t)
-            else:
-                await client.send_message(entity='target_channel', message=multiline_str_t)
-        if len(list_f_sorted) > 0:
-            if DEBUG_MODE:
-                print(f"==> Отчёты, не прошедшие по формату ({len(list_f_sorted)}):")
-            else:
-                await client.send_message(entity='target_channel', message=f" ==> Отчёты, не прошедшие по формату ({len(list_f_sorted)}):")
-            for item_f in list_f_sorted:
-                multiline_str_f = "\n".join(["{1}".format(i + 1, person_f) for i, person_f in enumerate(list_f_sorted)])
-            if DEBUG_MODE:
-                print(multiline_str_f)
-            else:
-                await client.send_message(entity='target_channel', message=multiline_str_f)
-run(messages_func(name, api_id, api_hash))
+        res = []
+        res = form_list(list_a, formatted_cpt, 'Отчёты от участников', res)
+        res = form_list(list_tf, formatted_cpt, 'Отчёты, не прошедшие и по формату и по дате', res)
+        res = form_list(list_t, formatted_cpt, 'Отчёты, не прошедшие по дате', res)
+        res = form_list(list_f, formatted_cpt, 'Отчёты, не прошедшие по формату', res)
+        return res
+
+
+result = messages_func(name, api_id, api_hash)
+run(send_comments(name, api_id, api_hash, result))
